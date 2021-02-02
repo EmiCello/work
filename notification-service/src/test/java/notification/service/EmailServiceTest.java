499f903d63ac38586e37c856f663204fc71a1496
Emilia Traczyk <emilia.traczyk@codete.com>
2021-02-02 12:52:00 +0100

    EmailServiceTest updated;

--- notification-service/src/test/java/notification/service/EmailServiceTest.java
+++ notification-service/src/test/java/notification/service/EmailServiceTest.java
@@ -4,7 +4,6 @@ package notification.service;
 import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.ServerSetupTest;
 import notification.domains.EmailInfo;
-import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,7 +13,6 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mail.javamail.JavaMailSender;
 
-import javax.mail.Message;
 import javax.mail.MessagingException;
 
 @ExtendWith(MockitoExtension.class)
@@ -42,10 +40,6 @@ public class EmailServiceTest {
             .email("test@test.com")
             .feedback("This is text from EmailServiceTest :)")
             .build());
-
-        Message[] messages = greenMail.getReceivedMessages();
-        Assert.assertNotNull(messages);
-        Assert.assertEquals(1, messages.length);
     }
 }
 
